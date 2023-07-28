71d7f56e9f39ae15b29a5dd748a652e2bb7a8f8c
shengyin-git <shengyin.wang@hotmail.com>
2023-07-28 16:04:54 +0100

    -

--- test_nn_pytorch_bce.py
+++ test_nn_pytorch_bce.py
@@ -337,15 +337,16 @@ for epoch in range(n_epochs):
 ##
 
 def inference(test_data):
-  idx = torch.randint(1, len(test_data), (1,))
-  sample1 = torch.unsqueeze(test_data[idx][0], dim=0).to(device)
-  sample2 = torch.unsqueeze(test_data[idx][1], dim=0).to(device)
-#   sample = torch.unsqueeze(test_data[idx][0], dim=0).to(device)
+#   idx = torch.randint(1, len(test_data), (1,))
+  for idx in range(len(test_data))
+    sample1 = torch.unsqueeze(test_data[idx][0], dim=0).to(device)
+    sample2 = torch.unsqueeze(test_data[idx][1], dim=0).to(device)
+    #   sample = torch.unsqueeze(test_data[idx][0], dim=0).to(device)
 
-  if torch.sigmoid(net(sample1,sample2)) < 0.5:
-    print("Prediction : Cat")
-  else:
-    print("Prediction : Dog")
+    if torch.sigmoid(net(sample1,sample2)) < 0.5:
+        print("Prediction : Cat")
+    else:
+        print("Prediction : Dog")
 
 
 #   plt.imshow(test_data[idx][0].permute(1, 2, 0))
