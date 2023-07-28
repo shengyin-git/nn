beecaf8117bc2aeb8b12d2cade0422ead3556de6
shengyin-git <shengyin.wang@hotmail.com>
2023-07-28 15:48:16 +0100

    -

--- test_nn_pytorch_bce.py
+++ test_nn_pytorch_bce.py
@@ -113,7 +113,7 @@ class SiameseNetworkDataset(Dataset):
 # Load the training dataset
 # Resize the images and transform to tensors
 # train_, val_, tes_ = split_train_val_tes(file_path='./data/train/*', num_=[1500,150,0])
-train_, val_, tes_ = split_train_val_tes(file_path, ratio_=[0.7,0.15,0.15])
+train_, val_, tes_ = split_train_val_tes(file_path='./data/train/*', ratio_=[0.7,0.15,0.15])
 transformation = transforms.Compose([transforms.Resize((100,100)),
                                      transforms.ToTensor()
                                     ])
